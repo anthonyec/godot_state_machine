4787ceacdb2bbb1f601853b218b3b17cd561f24b
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-06-17 16:41:31 +0100

    Add initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Godot State Machine
+
+Simple code only state machine that's is originally based off [GDQuest's implementation](https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine/).
+
+## Features (or problems)
+- States are nodes
+- Nested states, but only 1 level (parent and child)
+- Support for deferred state transitions
+- Bulk of the logic is in one file (under 200 lines)
+
+
+
---
+++ addons/anthonyec.state_machine/state.gd
@@ -0,0 +1,31 @@
+class_name State
+extends Node
+
+var state_machine: StateMachine = null
+var parent_state: State = null
+
+# TODO: Add ignore for unused variable
+func enter(_params: Dictionary) -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func awake() -> void:
+	pass
+
+# TODO: Add ignore for unused variable
+func update(_delta: float) -> void:
+	pass
+
+# TODO: Add ignore for unused variable
+func physics_update(_delta: float) -> void:
+	pass
+
+# TODO: Add ignore for unused variable
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+# TODO: Add ignore for unused variable
+func handle_message(_title: String, _params: Dictionary) -> void:
+	pass
